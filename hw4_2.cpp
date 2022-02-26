75fccd57b2f64b76f5e863eab54e6624cb249505
Thomas <thomas@localhost.localdomain>
2022-02-25 21:49:07 -0500

    Added comments to hw4_2.cpp

--- hw4_2.cpp
+++ hw4_2.cpp
@@ -3,8 +3,10 @@
 #include <string>
 
 int main(){
+	// initialize vector
 	std::vector<std::string> words;
 	
+	// read words from user 
 	std::string temp;
 	for(int i=0; i<10; ++i){
 		std::printf("Word %d: ", i+1);
@@ -12,12 +14,15 @@ int main(){
 		words.push_back(temp);
 	}
 
+	// find longest
 	std::string max_str = "";
 	for(int i=0; i<10; ++i){
 		if(words[i].length() > max_str.length()){
 			max_str = words[i];
 		}
 	}
+
+	// new line in from for when cin takes multiple words and doesn't terminate with a newline
 	std::printf("\n%s\n", max_str.c_str());
 	return 0;
 }
